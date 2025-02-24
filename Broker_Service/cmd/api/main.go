a276196558b3652b5cbd15e73baedb5d96df2322
Tanmoy095 <adtanmoy95@gmail.com>
2025-02-25 05:10:53 +0600

    implement broker service

---
+++ Broker_Service/cmd/api/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "80"
+
+type Config struct {
+}
+
+func main() {
+	app := Config{}
+
+	log.Printf("starting broker service on port %s\n", webPort)
+	//definr http server
+	srv := &http.Server{
+		Addr:    fmt.Sprintf("%:s", webPort),
+		Handler: app.routes(),
+	}
+
+	//start http server
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+
+	}
+
+}
